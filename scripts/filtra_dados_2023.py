b5d3c03a97d5e7b3869b0dc34dce046bfac728b2
Erik Gaborim <erik.pereira@discover.com.br>
2025-09-10 00:16:49 -0300

    limpando registros indesejados a partir do recorte do DF para o estado de SP - esta limpexa consiste em seguir as especificacoes do arquivo colunas_para_filtrar.txt

---
+++ scripts/filtra_dados_2023.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+df1 = pd.read_csv("../Dados_Saeb_2023/DADOS/SP_TS_ALUNO_34EM_parte_005.csv")
+df2 = pd.read_csv("../Dados_Saeb_2023/DADOS/SP_TS_ALUNO_34EM_parte_006.csv")
+df3 = pd.read_csv("../Dados_Saeb_2023/DADOS/SP_TS_ALUNO_34EM_parte_007.csv")
+
+df1_filtrado = df1[
+    (df1["IN_PREENCHIMENTO_LP"] == 1) &
+    (df1["IN_PREENCHIMENTO_MT"] == 1) &
+    (df1["IN_PRESENCA_LP"] == 1) &
+    (df1["IN_PRESENCA_MT"] == 1) &
+    (df1["IN_PROFICIENCIA_LP"] == 1) &
+    (df1["IN_PROFICIENCIA_MT"] == 1) &
+    (df1["IN_PREENCHIMENTO_QUESTIONARIO"] == 1)
+]
+
+df2_filtrado = df2[
+    (df2["IN_PREENCHIMENTO_LP"] == 1) &
+    (df2["IN_PREENCHIMENTO_MT"] == 1) &
+    (df2["IN_PRESENCA_LP"] == 1) &
+    (df2["IN_PRESENCA_MT"] == 1) &
+    (df2["IN_PROFICIENCIA_LP"] == 1) &
+    (df2["IN_PROFICIENCIA_MT"] == 1) &
+    (df2["IN_PREENCHIMENTO_QUESTIONARIO"] == 1)
+]
+
+df3_filtrado = df3[
+    (df3["IN_PREENCHIMENTO_LP"] == 1) &
+    (df3["IN_PREENCHIMENTO_MT"] == 1) &
+    (df3["IN_PRESENCA_LP"] == 1) &
+    (df3["IN_PRESENCA_MT"] == 1) &
+    (df3["IN_PROFICIENCIA_LP"] == 1) &
+    (df3["IN_PROFICIENCIA_MT"] == 1) &
+    (df3["IN_PREENCHIMENTO_QUESTIONARIO"] == 1)
+]
+
+df_filtrado_mesclado = pd.concat([df1_filtrado, df2_filtrado, df3_filtrado], ignore_index=True)
+
+cols_tx_resp = [c for c in df_filtrado_mesclado.columns if c.startswith("TX_RESP")]
+
+if cols_tx_resp:
+    df_filtrado_mesclado = df_filtrado_mesclado[~df_filtrado_mesclado[cols_tx_resp].isin(["*", "."]).any(axis=1)]
+
+df_filtrado_mesclado.to_csv("dados_filtrados_2023.csv", index=False)
+
+print("Arquivo 'dados_filtrados_2023.csv' gerado com sucesso!")
