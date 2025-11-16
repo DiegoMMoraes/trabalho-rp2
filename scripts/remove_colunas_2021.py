a973f1899f3f10dc6dd70c8b2e3aeabe19c4ff3e
Diego Moraes <diego.machado.moraes@gmail.com>
2025-11-16 19:54:02 -0300

    commit final

--- scripts/remove_colunas_2021.py
+++ scripts/remove_colunas_2021.py
@@ -1,9 +1,25 @@
 import pandas as pd
 
-colunas_para_remover = ['Unnamed: 0','ID_SAEB', 'ID_REGIAO', 'ID_MUNICIPIO', 'ID_ESCOLA', 'ID_TURMA', 'ID_SERIE', 'IN_SITUACAO_CENSO', 'IN_PREENCHIMENTO_LP', 'IN_PREENCHIMENTO_MT', 'IN_PRESENCA_LP', 'IN_PRESENCA_MT', 'ID_CADERNO_LP', 'ID_BLOCO_1_LP', 'ID_BLOCO_2_LP', 'ID_CADERNO_MT', 'ID_BLOCO_1_MT', 'ID_BLOCO_2_MT', 'TX_RESP_BLOCO_1_LP', 'TX_RESP_BLOCO_2_LP', 'TX_RESP_BLOCO_1_MT', 'TX_RESP_BLOCO_2_MT', 'IN_PROFICIENCIA_LP', 'IN_PROFICIENCIA_MT', 'IN_AMOSTRA', 'ESTRATO', 'PESO_ALUNO_LP', 'PROFICIENCIA_LP', 'ERRO_PADRAO_LP', 'PESO_ALUNO_MT', 'PROFICIENCIA_MT', 'ERRO_PADRAO_MT', 'IN_PREENCHIMENTO_QUESTIONARIO', 'IN_INSE', 'INSE_ALUNO']
+# Lista atualizada para remover colunas de ID, proficiência E perguntas não-comuns (Q23)
+colunas_para_remover = [
+    'Unnamed: 0', 'ID_SAEB', 'ID_REGIAO', 'ID_MUNICIPIO', 'ID_ESCOLA', 'ID_TURMA', 'ID_SERIE',
+    'IN_SITUACAO_CENSO', 'IN_PREENCHIMENTO_LP', 'IN_PREENCHIMENTO_MT', 'IN_PRESENCA_LP', 'IN_PRESENCA_MT',
+    'ID_CADERNO_LP', 'ID_BLOCO_1_LP', 'ID_BLOCO_2_LP', 'ID_CADERNO_MT', 'ID_BLOCO_1_MT', 'ID_BLOCO_2_MT',
+    'TX_RESP_BLOCO_1_LP', 'TX_RESP_BLOCO_2_LP', 'TX_RESP_BLOCO_1_MT', 'TX_RESP_BLOCO_2_MT',
+    'IN_PROFICIENCIA_LP', 'IN_PROFICIENCIA_MT', 'IN_AMOSTRA', 'ESTRATO', 'PESO_ALUNO_LP', 'PROFICIENCIA_LP',
+    'ERRO_PADRAO_LP', 'PESO_ALUNO_MT', 'PROFICIENCIA_MT', 'ERRO_PADRAO_MT', 'IN_PREENCHIMENTO_QUESTIONARIO',
+    'IN_INSE', 'INSE_ALUNO',
+    # Adicionando perguntas não-comuns (Q23 - Pandemia)
+    'TX_RESP_Q23a', 'TX_RESP_Q23b', 'TX_RESP_Q23c', 'TX_RESP_Q23d',
+    'TX_RESP_Q23e', 'TX_RESP_Q23f', 'TX_RESP_Q23g', 'TX_RESP_Q23h', 'TX_RESP_Q23i' # 'i' não existe em 2021, mas não causa erro
+]
 
 df = pd.read_csv('Dados_Saeb_2021/DADOS/dados_filtrados_2021.csv', dtype=str)
+
+# Manter apenas colunas que NÃO ESTÃO na lista de remoção
 colunas_para_manter = [col for col in df.columns if col not in colunas_para_remover]
 df = df[colunas_para_manter]
+
+print("Colunas mantidas no SAEB 2021:")
 print(df.info())
-df.to_csv('dados_filtrados_sem_coluna.csv')
+df.to_csv('dados_filtrados_comuns_2021.csv')
